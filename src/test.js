b66af014345ceba6ab382c5a3da0473bac675c8e
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-19 17:32:16 +0530

    added a test file to check the tree creation

---
+++ src/test.js
@@ -0,0 +1,20 @@
+import Tree from './binary_tree.mjs';
+
+function Test() {
+    let tree = new Tree();
+    tree.root = tree.buildTree([1,4,5,7,8], 0, 4);
+    prettyPrint(tree.root);
+    // console.log(tree);
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
+
+Test();
