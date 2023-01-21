2e0f6b940784f63f93fe0ac951867d09ced2dcaf
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-21 20:46:28 +0530

    modified the test mjs

--- src/test.mjs
+++ src/test.mjs
@@ -1,22 +1,53 @@
 import Tree from './binary_tree.mjs';
 
 function Test() {
-    let tree = new Tree();
-    tree.root = tree.buildTree([1,4,5,7,8], 0, 4);
-    prettyPrint(tree.root);
+    let tree = new Tree([1,4,5,5,7,8,11,13]);
+    tree.prettyPrint();
+    console.log(tree.isBalanced());
 
-    tree.insert(6);
-    prettyPrint(tree.root);
+    tree.insert(2);
+    tree.prettyPrint();
+    console.log(tree.height());
+
+    tree.insert(3);
+    tree.prettyPrint();
+    console.log(tree.height());
+
+    tree.delete(4);
+    tree.prettyPrint();
+
+    console.log(tree.find(5).data);
+
+    console.log(tree.levelOrder());
+    console.log(tree.levelOrderRecursion());
+
+    tree.inorder().forEach(ele => console.log(ele.data));
+    console.log('---------------------------------------');
+    tree.preorder().forEach(ele => console.log(ele.data));
+    console.log('---------------------------------------');
+    tree.postorder().forEach(ele => console.log(ele.data));
+    console.log('---------------------------------------');
+
+    console.log(tree.height());
+
+    console.log('depth of node with value 5 is ' + tree.depth(tree.find(5)));
+
+    console.log(tree.isBalanced());
+
+    tree.prettyPrint();
+    console.log('rebalance tree');
+    tree.rebalance();
+    tree.prettyPrint();
 }
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-}
+// const prettyPrint = (node, prefix = '', isLeft = true) => {
+//     if (node.right !== null) {
+//       prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+//     }
+//     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+//     if (node.left !== null) {
+//       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//     }
+// }
 
 Test();
